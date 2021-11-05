58e99ed1a444387b57cac2f178cf7d4e5c7bdf45
SwiTool <switooldev@gmail.com>
2021-11-05 14:34:59 +0100

    handle

--- src/Events/TranslationsExportedEvent.php
+++ src/Events/TranslationsExportedEvent.php
@@ -3,15 +3,13 @@
  * Created by PhpStorm.
  * User: kgbot
  * Date: 5/29/18
- * Time: 12:40 PM
+ * Time: 12:40 PM.
  */
 
 namespace Barryvdh\TranslationManager\Events;
 
-
 class TranslationsExportedEvent
 {
-
     /**
      * Create a new event instance.
      *
@@ -19,6 +17,9 @@ class TranslationsExportedEvent
      */
     public function __construct()
     {
+    }
 
+    public function handle()
+    {
     }
 }
